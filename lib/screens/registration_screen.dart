8b14e5322076566d48558b4efdc8282d35041485
f1rstCoder <ritwikcoding@gmail.com>
2022-06-14 15:13:25 +0530

    second commit

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -175,7 +175,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           'SignUp',
           textAlign: TextAlign.center,
           style: TextStyle(
-              fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
+              fontSize: 20, color: Colors.yellow, fontWeight: FontWeight.bold),
         ),
       ),
     );
